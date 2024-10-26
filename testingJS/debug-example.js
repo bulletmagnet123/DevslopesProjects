bdde64d32f31c88bf059b961c4a126383f387b31
bulletmagnet <bulletmagnet.123@gmail.com>
2024-10-25 17:36:25 -0700

    added projects

---
+++ testingJS/debug-example.js
@@ -0,0 +1,65 @@
+// var tvShows = [
+//   'The walking dead',
+//   'Breaking bad',
+//   'The office',
+// ];
+
+// var userSavedMovies = []
+
+// var toDos = [
+//   {title: 'item1', completed: true},
+//   {title: 'item2', completed: false},
+//   {title: 'item3', completed: false},
+//   {title: 'item4', completed: true},
+  
+// ]
+
+
+// // for (var i = 0; i < tvShows.length; i++){
+// //   console.log(tvShows[i])
+// // }
+
+// console.log(toDos)
+
+// var completedItems = []
+
+// for (var x = 0; x < toDos.length; x++){
+//   if (toDos[x].completed){
+//     completedItems.push(toDos)
+//   }
+// }
+
+// console.log(toDos)
+// console.log(completedItems)
+
+// var bottles = 99
+
+// while (bottles > 0){
+//   if (bottles !== 1){
+//     console.log(bottles)
+//   }
+// }
+// function calcArea(width, length){
+//     return width * length
+// }
+
+// function toCelsius(f){
+//   var temp = (5/9) * (f - 32)
+//   return temp.toFixed(2)
+// }
+
+// var currentTempF = 76
+
+// var weather = 'The temp outside is ' + toCelsius(currentTempF) + ' celsius!'
+
+// console.log(weather)
+
+
+
+// var kitchenArea = calcArea(15, 18)
+// var bedroomArea = calcArea(12, 10)
+
+// console.log(kitchenArea)
+// console.log(bedroomArea)
+
+
